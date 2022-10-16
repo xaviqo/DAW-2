159dd39f80d3db200289c325ce4c36fc93661690
xaviqo <silent90s@gmail.com>
2022-10-16 10:27:25 +0200

    WIP

--- PHP/COCHES/index.php
+++ PHP/COCHES/index.php
@@ -22,32 +22,70 @@ const COCHES = array(
     )
 );
 
-$gasolineras = array(
-    array(
-        "litros" => 1000,
-        "color" => "azul"
-    ),
-    array(
-        "litros" => 1000,
-        "color" => "verde"
-    ),
-    array(
-        "litros" => 1000,
-        "color" => "rojo"
-    )
-);
+if (isset($_REQUEST['camion'])){
+    $_SESSION['gasolineras'] = null;
+}
 
-if (isset($_SESSION['cola'])){
+//TODO cuando camion, reset.
+if (!isset($_SESSION['gasolineras']) || $_SESSION['gasolineras'] == null) {
+    $gasolineras = array(
+        array(
+            "litros" => 1000,
+            "color" => "azul"
+        ),
+        array(
+            "litros" => 1000,
+            "color" => "verde"
+        ),
+        array(
+            "litros" => 1000,
+            "color" => "rojo"
+        )
+    );
+} else {
+    $gasolineras = $_SESSION['gasolineras'];
+}
+
+if (isset($_SESSION['cola'])) {
     $cola = $_SESSION['cola'];
-} 
 
-// crear cola coches
-if (sizeof($cola) < MAX_COLA) {
-    for ($i=sizeof($cola); $i < MAX_COLA; $i++) { 
-        $cola[$i] = generateCar();
+    if ((sizeof($cola) > 3) && isset($_REQUEST['camion'])) {
+        if ($_REQUEST['camion']) $cola = deleteLastCar($cola);
+    }
+
+    if (isset($_REQUEST['color'])) {
+        $colorReq = $_REQUEST['color'];
+        $lastCar = getLastCar($cola);
+        if ($colorReq == $lastCar['color'] && $_REQUEST['litros'] > 0) {
+            $llenado = $_REQUEST['litros'];
+            // check litros gas disponibles
+            $pickedGasolinera = getGasolinera($colorReq, $gasolineras);
+            // update gasolinera
+            if ($llenado <= $pickedGasolinera['litros']) {
+                $pickedGasolinera['litros'] = $pickedGasolinera['litros']-$llenado;
+                $gasolineras = updateGasolinera($pickedGasolinera, $gasolineras);
+            }
+        }
+        //  update car
+        $totalDeposito = $lastCar['deposito'] + $llenado;
+
+        if ($lastCar['maxDeposito'] >= $totalDeposito) {
+            $lastCar['deposito'] = $totalDeposito;
+        }
+
+        $cola = updateLastCar($lastCar, $cola);
     }
 }
+
+
+
+
+
+
+$cola = fillCola($cola);
+
 $_SESSION['cola'] = $cola;
+$_SESSION['gasolineras'] = $gasolineras;
 
 
 ?>
@@ -60,7 +98,7 @@ $_SESSION['cola'] = $cola;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gasolinera</title>
     <style>
-        .coche{
+        .coche {
             margin: 60px;
         }
     </style>
@@ -68,29 +106,31 @@ $_SESSION['cola'] = $cola;
 
 <body>
     <?php
-        foreach ($cola as $coche) {
-            echo '<div style="display: inline-block; margin: 10px;">';
-            printHtml($coche['color'],"margin-right: 10px; ");
-            echo '<div>Depósito: '.$coche['deposito'].'/'.$coche['maxDeposito'].'</div>';
-            echo '</div>';
-        }
+    foreach ($cola as $coche) {
+        echo '<div style="display: inline-block; margin: 10px;">';
+        printHtml($coche['color'], "margin-right: 10px; ");
+        echo '<div>Depósito: ' . $coche['deposito'] . '/' . $coche['maxDeposito'] . '</div>';
+        echo '</div>';
+    }
 
-        foreach ($gasolineras as $gas) {
-            echo '<div style="display: inline-block; margin: 20px;">';
-            printHtml('gas-'.$gas['color'],"width: 100px;");
-            echo BR.'<strong>Litros:</strong>: '.$gas['litros'];
-            ?>
-            <form>
-                <?php
-                echo '<input type="hidden" name="gas" value="'.$gas['color'].'">';
-                ?>
-                    <input type="number" name="" style="width: 40px;">€
-                    <input type="button" value="Pagar">
-            </form>
+    foreach ($gasolineras as $gas) {
+        echo '<div style="display: inline-block; margin: 20px;">';
+        printHtml('gas-' . $gas['color'], "width: 100px;");
+        echo BR . '<strong>Litros:</strong>: ' . $gas['litros'];
+    ?>
+        <form action="index.php">
             <?php
-            echo '</div>';
-        }
-        echo '<img src="/img/camion.png" style="margin-left:15px; width: 200px"></img>'
+            echo '<input type="hidden" name="color" value="' . $gas['color'] . '">';
+            ?>
+            <input type="number" name="litros" style="width: 40px;">€
+            <input type="submit" value="Pagar">
+        </form>
+    <?php
+        echo '</div>';
+    }
+    echo '<a href="index.php?camion=true">';
+    echo '<img src="/img/camion.png" style="margin-left:15px; width: 200px"></img>';
+    echo '</a>';
     ?>
 </body>
 
@@ -98,14 +138,62 @@ $_SESSION['cola'] = $cola;
 
 <?php
 
-    function printHtml($var,$style){
-        echo '<img src="/img/'.$var.'.png" style="'.$style.'"></img>';
+function printHtml($var, $style)
+{
+    echo '<img src="/img/' . $var . '.png" style="' . $style . '"></img>';
+}
+
+function generateCar()
+{
+    $coche = COCHES[rand(0, 2)];
+    $coche['maxDeposito'] = rand(90, 100);
+    return $coche;
+}
+
+function updateLastCar($car, $cola)
+{
+    $cola[3] = $car;
+    return $cola;
+}
+
+function deleteLastCar($cola)
+{
+    array_pop($cola);
+    return $cola;
+}
+
+function getLastCar($cola)
+{
+    return $cola[3];
+}
+
+function fillCola($cola)
+{
+    if (sizeof($cola) < MAX_COLA) {
+
+        for ($i = sizeof($cola); $i < MAX_COLA; $i++) {
+            array_unshift($cola, generateCar());
+        }
     }
 
-    function generateCar(){
-        $coche = COCHES[rand(0,2)];
-        $coche['maxDeposito'] = rand(90,100);
-        return $coche;
-    }
+    return $cola;
+}
 
+function getGasolinera($color, $gasolineras)
+{
+    foreach ($gasolineras as $gasolinera) {
+        if ($gasolinera['color'] == $color) return $gasolinera;
+    }
+}
+
+function updateGasolinera($newGasolinera, $gasolineras)
+{
+    foreach ($gasolineras as $key => $gasolinera) {
+        if ($gasolinera['color'] == $newGasolinera['color']) {
+            $gasolineras[$key] = $newGasolinera;
+            var_dump($gasolineras[$key]);
+        }
+    }
+    return $gasolineras;
+}
 ?>
