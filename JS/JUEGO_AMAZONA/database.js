431aae251e421cfaa10ebcc92821c961d1d89878
xaviqo <silent90s@gmail.com>
2023-01-15 20:55:12 +0100

    WIP

---
+++ JS/JUEGO_AMAZONA/database.js
@@ -0,0 +1,13 @@
+const Move = {
+    Pick: "pick",
+    Move: "move",
+    Shoot: "shoot"
+}
+
+const game = {
+    movement: Move.Pick,
+    player: 'one',
+    movingFigure: null
+}
+
+export { Move, game } ;
