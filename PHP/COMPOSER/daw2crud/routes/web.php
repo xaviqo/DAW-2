238c2b77f356431c2443f554d601bc0988069403
xaviqo <silent90s@gmail.com>
2023-01-31 20:13:25 +0100

    WIP

--- PHP/COMPOSER/daw2crud/routes/web.php
+++ PHP/COMPOSER/daw2crud/routes/web.php
@@ -14,22 +14,4 @@ use App\Http\Controllers\BookController;
 |
 */
 
-<<<<<<< HEAD:PHP/COMPOSER/daw2crud/routes/web.php
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('book',BookController::class);
-
-// Route::get('/books/create', [BookController::class, 'create']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-=======
 Route::get('/', [\App\Http\Controllers\ControllerProducto::class,'getAllProductos']);
->>>>>>> 14fdf4c50d0bdbe88c219c265ecdb8cc5f5b3e6f:PHP/COMPOSER/test/routes/web.php
